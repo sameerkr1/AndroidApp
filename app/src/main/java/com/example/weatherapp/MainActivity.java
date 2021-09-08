74d72efbe2110a91b6a4d2910799a4a3345174d8
Sameer kumar singh <sameerkumarsingh123456@gmail.com>
2021-09-08 22:53:48 +0530

    web_view

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,41 +27,28 @@ import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnWeather;
-    TextView tvDisplay;
-    EditText etCity;
-
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnWeather = findViewById(R.id.btnWeather);
-        tvDisplay = findViewById(R.id.tvDisplay);
-        etCity = findViewById(R.id.etCity);
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
 
     }
 
-    public void get(View v) {
-        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + etCity.getText().toString() + "&appid=" + "9fb611d0a3d8e37b47883276b0ccb617";
-        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-
-        @SuppressLint("SetTextI18n") JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, response -> {
-            try {
-                DecimalFormat df = new DecimalFormat("#.##");
-                JSONObject jsonObject = response.getJSONObject("main");
-                String temp = jsonObject.getString("temp");
-                Double tempDouble = Double.parseDouble(temp) - 273.15;
-                tvDisplay.setText("Temperature of " + etCity.getText() + " is " + df.format(tempDouble));
-            } catch (JSONException e) {
-                e.printStackTrace();
-                Toast.makeText(getApplicationContext(), "something went wrong1", Toast.LENGTH_SHORT).show();
-            }
-        }, error -> {
-            System.out.println(error.toString());
-            Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
-        });
-        queue.add(jsonObjectRequest);
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
